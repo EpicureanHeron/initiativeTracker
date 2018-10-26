5cec1a88b25166937d5ad87a0ec1c579a446c71d
Joe Thor <joethor1989@gmail.com>
2018-10-26 09:51:51 -0500

    began work on Battle.js mongoose model. Added an association back to Character.js for player characters.

--- models/Battle.js
+++ models/Battle.js
@@ -5,48 +5,24 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-const RecipeSchema = new Schema({
+const BattleSchema = new Schema({
   // `title` is required and of type String
-  recipeName: {
-    type: String,
-    required: true
+  turnNumber: {
+    type: Number,
+    required: true,
+    default: 1
   },
   // `link` is required and of type String
-  image: {
-    type: String,
-    required: true
-  },
-
-  description: { 
-    type: String, 
-    
-   required: true
-   },
-
-   ingredients: {
-     type: String,
-     required: true,
-   },
-   primaryMeat:{
-     type:String,
-     enum: ['Pork', 'Beef', 'Fish', 'Chicken', 'Turkey', 'Other', 'Lamb'],
-     required: true,
-   },
-   count:{
-    type: Number,
-    default: 0,
-    required: true
-   },
+ 
    //this may not need to be a string 
-   user:{
-     type: String,
-     required: true,
-   }
+   playerCharacters : [{ type : Schema.Types.ObjectId, ref: 'Character' }],
+
+
 
 });
 
-const Recipe = mongoose.model("Recipe", RecipeSchema);
+const Battle = mongoose.model("Battle", BattleSchema);
 
 
 // Export the Article model
-module.exports = Recipe;
+module.exports = Battle;
