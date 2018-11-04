7e503c9c51a163a45cf8b9757b506561abafbd17
Joe Thor <joethor1989@gmail.com>
2018-11-04 10:33:48 -0600

    button added to battles page that sorts by the current init. Should able to do a bit more work to calculate init in that function and then use it as the map.

--- client/src/pages/battle.js
+++ client/src/pages/battle.js
@@ -14,8 +14,28 @@ class CharPage extends React.Component {
   componentDidMount() {
     console.log("mounted!")
     API.getAllCharacter()
-    .then(res => this.setState({ PCarray: res.data }))
+    .then(res => this.setState({ PCarray: res.data}))
   }
+
+  initUpdate(id) {
+    let init = prompt("Enter init")
+    init = parseInt(init)
+    let update = {
+      currentInitRoll: init
+    }
+    API.updateInit(id, update)
+      .then(res => console.log(res.data))
+  }
+
+
+
+  initSort(numArray) {
+
+  //  return a.currentInitRoll.localeCompare(b.currentInitRoll);
+   let newArray = numArray.sort((a, b) => a.currentInitRoll- b.currentInitRoll);
+   console.log(newArray)
+  }
+
 
   render() {
     return (
@@ -23,14 +43,17 @@ class CharPage extends React.Component {
 
       <Wrapper>
         <p>This is the battle page</p>
-        {(this.state.PCarray).map(item => <CharCard
+           {(this.state.PCarray).map(item => <CharCard
+            initUpdate={this.initUpdate}
+            currentInitRoll = {item.currentInitRoll}
             key={item._id}
-            dex = {item.dex}
+            dex={item.dex}
             id={item._id}
             name={item.name}
-            player = {item.player}
+            player={item.player}
             image={item.image} />)}
       </Wrapper>
+      <button type="button" onClick={() => this.initSort(this.state.PCarray)} class="btn btn-primary">Primary</button>
     </div>
     )
   }
