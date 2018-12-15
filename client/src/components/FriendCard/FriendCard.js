9a088e53249d35ce874923e55c29d1e31cbd32c0
Joe Thor <joethor1989@gmail.com>
2018-12-14 21:11:09 -0600

    added some obv flag

--- client/src/components/FriendCard/FriendCard.js
+++ client/src/components/FriendCard/FriendCard.js
@@ -2,15 +2,39 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card" data-id={props.id} onClick={() => props.initUpdate(props.id)} >
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
-    <p>{props.name}</p>
-    <p>Current Init Roll {props.currentInitRoll}</p>
-    <p>Dex: {props.dex}</p>
-    <p>Calculate init roll: {props.currentInitRoll + props.dex}</p>
-    <p>Status: {props.status.toString()}</p>
+  <div>
+
+    {(props.active === true)
+    
+      ? 
+
+      <div className="activePlayer card" data-id={props.id} onClick={() => props.initUpdate(props.id)} >
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+        <p>{props.name}</p>
+        <p>Current Init Roll {props.currentInitRoll}</p>
+        <p>Dex: {props.dex}</p>
+        <p>Calculate init roll: {props.currentInitRoll + props.dex}</p>
+        <p>Status: {props.status.toString()}</p>
+        <p>active boy</p>
+      </div>
+
+      : 
+      
+      <div className="card" data-id={props.id} onClick={() => props.initUpdate(props.id)} >
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+        <p>{props.name}</p>
+        <p>Current Init Roll {props.currentInitRoll}</p>
+        <p>Dex: {props.dex}</p>
+        <p>Calculate init roll: {props.currentInitRoll + props.dex}</p>
+        <p>Status: {props.status.toString()}</p>
+      </div>
+    }
+
+
   </div>
 );
 
