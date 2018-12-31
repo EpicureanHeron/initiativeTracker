9a936b59cae00382cafd4ef217cc6175cb772447
Joe Thor <joethor1989@gmail.com>
2018-12-31 09:47:25 -0600

    tried adding a ternary function, but still failing to render because it cannot find props.name or props.id...which is frustrating

--- client/src/components/InitForm/InitForm.js
+++ client/src/components/InitForm/InitForm.js
@@ -1,49 +1,49 @@
 import React, { Component } from "react";
 import "./InitForm.css";
-// import API from "./../../utils/API"
+import API from "./../../utils/API"
 
-// class InitForm extends React.Component { 
+class InitForm extends Component {
 
-//   state = {
-//     name: "",
-//     dex: "",
-//     playerImage: "",
-//     player: ""
-//   };
+  state = {
+    name: "",
+    dex: "",
+    playerImage: "",
+    player: ""
+  };
 
-//   handleInputChange = event => {
-//     const { name, value } = event.target;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-
-// render = props => {
-//   return (
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
 
-//       <div>
-//         <p>{props.name}</p>
-//       {/* <input name ="name" value={this.state.name} onChange={this.handleInputChange} type="name" className="form-control custom-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder={props.name}  /> */}
-//         <input id={props._id}></input>
-//       </div>
+  render = props => {
+    return (
 
 
 
 
 
-//     )
-//   }
-// }
+      <div>
+        {(props.name)
+          ?
+          <div>
 
-const InitForm = props => (
+            <p>{props.name}</p>
 
-  <div>
-    <p>{props.name}</p>
-    {/* <input name ="name" value={this.state.name} onChange={this.handleInputChange} type="name" className="form-control custom-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder={props.name}  /> */}
-    <input id={props._id}></input>
-  </div>
+            {/* <input name ="name" value={this.state.name} onChange={this.handleInputChange} type="name" className="form-control custom-form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder={props.name}  /> */}
+            <input id={props._id}></input>
 
-)
+          </div>
+
+          :
+          <div>Loading</div>
+        }
+      </div>
+    )
+  }
+}
+
 export default InitForm;
